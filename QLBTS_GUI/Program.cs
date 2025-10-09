3653655a0f305dc89833183b49abeac4ea42717e
Tai-02 <tai741760@gmail.com>
2025-10-09 22:29:48 +0700

    Edit NET 8.0

--- QLBTS_GUI/Program.cs
+++ QLBTS_GUI/Program.cs
@@ -11,6 +11,8 @@ namespace QLBTS_GUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Form form = new Form1();
+            form.ShowDialog();
         }
     }
 }
